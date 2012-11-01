f2dba28f513fa7ff41ea8d855853bcd8b24bfe89
Sébastien Andreu <seb@seb-ubuntu.(none)>
2012-11-01 11:42:23 +0100

    User data handler

---
+++ Lib/CakeRavenClient.php
@@ -0,0 +1,29 @@
+<?php
+
+App::uses('ClassRegistry', 'Utility');
+
+class CakeRavenClient extends Raven_Client {
+    public function capture($data, $stack) {
+    	if (class_exists('AuthComponent')) {
+    		$model= Configure::read('Sentry.User.model');
+    		if (empty($model)) $model = 'User';
+
+    		$User = ClassRegistry::init($model);
+
+    		$mail = Configure::read('Sentry.User.email_field');
+    		if (empty($mail)) {
+	    		if ($User->hasField('email')) $mail = 'email';
+	    		else if ($User->hasField('mail')) $mail = 'mail';
+	    	}
+
+    		$data['sentry.interfaces.User'] = array(
+				"is_authenticated" => AuthComponent::user($User->primaryKey) ? true : false,
+				"id" => AuthComponent::user($User->primaryKey),
+				"username" => AuthComponent::user($User->displayField),
+				"email" => AuthComponent::user($mail)
+    		);
+    	}
+
+    	return parent::capture($data, $stack);
+	}
+}
